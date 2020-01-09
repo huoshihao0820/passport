1c4a53d83c26be29e9bf7d29fe7e63543ebb8751
huo <huo@qq.com>
2020-01-09 08:27:08 +0000

    aa

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\models\RegiModel;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function register(){
+        $data=$_POST;
+        unset($data['_token']);
+        $res=RegiModel::where('email',$data['email'])->first();
+//        dd($res);
+        if ($res){
+            $arr=[
+                'code' =>201,
+                'msg'  =>'邮箱存在',
+            ];
+            echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }else{
+            if ($data['password']==$data['password2']){
+                unset($data['password2']);
+                RegiModel::insert($data);
+                $token=md5($data['email'].$data['password'].'huoshihao');
+//                dd($token);
+                $arr=[
+                    'code' =>200,
+                    'msg'  =>'注册成功',
+                    'token'=>$token,
+                ];
+                echo json_encode($arr);
+            }else{
+                $arr=[
+                    'code' =>202,
+                    'msg'  =>'密码不一致',
+                ];
+                return json_encode($arr);
+            }
+        }
+    }
+    public function login(){
+        $data=$_POST;
+        unset($data['_token']);
+        $res=RegiModel::where('email',$data['email'])->first();
+        if ($res){
+            if ($data['password']==$res->password){
+
+            }else{
+                $arr=[
+                    'code' =>204,
+                    'msg'  =>'密码不正确',
+                ];
+                echo json_encode($arr);
+            }
+        }else{
+            $arr=[
+                'code' =>203,
+                'msg'  =>'邮箱不存在',
+            ];
+            echo json_encode($arr);
+        }
+    }
+}
---
+++ app/models/RegiModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RegiModel extends Model
+{
+    protected $table='p_user';
+    protected $pk='id';
+}
